862129b13bb1f454e90ed5066293fbfcc098851e
Matthijs <matthijskroon@gmail.com>
2020-09-06 01:47:41 +0200

    remove unused var

--- example/src/components/SelectWithDispatch.js
+++ example/src/components/SelectWithDispatch.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { useContextState } from 'react-global-state'
 import Spacer from './Spacer'
 import style from './demo.module.css'
