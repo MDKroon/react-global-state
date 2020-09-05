8f43939e2acf3298651603a0b8108b2c573e1e52
Matthijs <matthijskroon@gmail.com>
2020-09-05 21:16:02 +0200

    add StateProvider and useContextState

--- src/index.js
+++ src/index.js
@@ -1,6 +1,74 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { createContext, useReducer, useContext } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const AppContext = createContext({})
+
+export const StateProvider = ({ initialState = {}, children }) => {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'UPDATE':
+        if (action.key) {
+          return {
+            ...state,
+            [action.name]: {
+              ...state[action.name],
+              [action.key]: action.value
+            }
+          }
+        } else {
+          return {
+            ...state,
+            [action.name]: action.value
+          }
+        }
+      case 'ADD':
+        if (action.key) {
+          return {
+            ...state,
+            [action.name]: {
+              ...state[action.name],
+              [action.key]: state[action.name][action.key] + action.value
+            }
+          }
+        } else {
+          return {
+            ...state,
+            [action.name]: state[action.name] + action.value
+          }
+        }
+      case 'RESET':
+        if (action.key && action.name) {
+          return {
+            ...state,
+            [action.name]: {
+              ...state[action.name],
+              [action.key]: initialState[action.name][action.key]
+            }
+          }
+        } else if (action.name) {
+          return {
+            ...state,
+            [action.name]: initialState[action.name]
+          }
+        } else {
+          return {
+            ...initialState
+          }
+        }
+      default:
+        throw new Error()
+    }
+  }, initialState)
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useContextState = () => {
+  const globalState = useContext(AppContext)
+  const { state, dispatch } = globalState
+
+  return { state, dispatch }
 }
