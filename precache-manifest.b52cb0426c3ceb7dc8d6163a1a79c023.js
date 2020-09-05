525a5d642ac20ce6ef2ab9a24a081d30e404966c
Matthijs <matthijskroon@gmail.com>
2020-09-06 01:39:52 +0200

    Updates

---
+++ precache-manifest.b52cb0426c3ceb7dc8d6163a1a79c023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37e659e4c9b9e5a30166abd56a60c74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ed025121180dd5e0946",
+    "url": "./static/css/main.07e7d8e8.chunk.css"
+  },
+  {
+    "revision": "24e3ce50dbac1302d97c",
+    "url": "./static/js/2.710c61f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.710c61f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed025121180dd5e0946",
+    "url": "./static/js/main.55e3be29.chunk.js"
+  },
+  {
+    "revision": "52eb7041f590127712a3",
+    "url": "./static/js/runtime-main.d3d792bc.js"
+  }
+]);
